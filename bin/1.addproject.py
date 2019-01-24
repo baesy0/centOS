547686ea3a7600d03ca25fa697ec07d14109b13f
bae <bsy2658@gmail.com>
2019-01-24 11:35:41 +0900

    1

--- bin/1.addproject.py
+++ bin/1.addproject.py
@@ -5,7 +5,13 @@ import os
 
 def addProject(name):
 	"""
+	프로젝트를 생성하는 함수입니다
 	"""
-
+	print name	
 if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print "How to use:"
+		print "$ addproject projectname"
+		sys.exit()
+	name = sys.argv[1]
 	addProject(name)
