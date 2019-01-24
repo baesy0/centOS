28bc9358e95dbb583efa387b5a6b4d685c5d2f53
bae <bsy2658@gmail.com>
2019-01-24 11:39:49 +0900

    2

--- bin/1.addproject.py
+++ bin/1.addproject.py
@@ -7,7 +7,23 @@ def addProject(name):
 	"""
 	프로젝트를 생성하는 함수입니다
 	"""
-	print name	
+	root = "/project"
+	dirs = ["shot",
+			"in",
+			"out",
+			"edit",
+			"asset",
+			"asset/char",
+			"asset/shader",
+			"config",
+			"config/ocio",
+			"config/thumbnail",
+			"doc",
+			"doc/concept"]
+
+	for d in dirs:
+		print root, name, d
+
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		print "How to use:"
