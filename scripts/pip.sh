00ba7cd4e71b81f33fb3837fce4a2e4c6bf6ea12
baesy0 <bsy2658@gmail.com>
2019-02-08 16:40:32 +0900

    install cython

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#/bin/sh
 #pip를 설치하기 위한 파일입니다
 
 cd /tmp
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 
-pip install --upgrade setuptools
+#pip install --upgrade setuptools
 pip install pymediainfo
+pip install --user numpy
+pip install --user Cython
