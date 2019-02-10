8f428f855905264e6ca10b799dd184bc75e7905a
baesy0 <bsy2658@gmail.com>
2019-02-10 15:41:30 +0900

    fix error

--- pEnv/install.sh
+++ pEnv/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "source /home/$USER/centOS/env/pInit.env" >> /home/$USER/.bashrc
-echo "source /home/$USER/centOS/env/pHelp.env" >> /home/$USER/.bashrc
+echo "source /home/$USER/centOS/pEnv/pInit.env" >> /home/$USER/.bashrc
+echo "source /home/$USER/centOS/pEnv/pHelp.env" >> /home/$USER/.bashrc
