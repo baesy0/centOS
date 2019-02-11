f561cde3fcedffaaeb4fb1681c678822a06f1667
baesy0 <bsy2658@gmail.com>
2019-02-11 11:08:25 +0900

    install excel library

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -9,3 +9,11 @@ python get-pip.py
 pip install pymediainfo
 pip install --user numpy
 pip install --user Cython
+
+#Excel parser
+  pip install --user openpyxl
+  pip install --user xlsxwriter
+  pip install --user xlrd
+  pip install --user xlwt
+  pip install --user xlutils
+  pip install --user pillow
