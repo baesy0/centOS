78e26c14145c321bb5946599c98a4a7593c1d823
baesy0 <bsy2658@gmail.com>
2019-02-13 11:18:43 +0900

    install gaffer, blender

---
+++ scripts/gaffer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ ! -d "/home/$USER/app" ]; then
+	mkdir /home/$USER/app
+fi
+
+#OpenEXR 소스코드 다운로드
+cd /home/$USER/app
+wget https://github.com/ImageEngine/gaffer/releases/download/0.53.0.0/gaffer-0.53.0.0-linux.tar.gz
+tar -zxvf gaffer-0.53.0.0-linux.tar.gz
+
